8aae63183fbdd4b4f51314a93492ed6dedf2e274
guo <389878212@qq.com>
2013-12-16 19:00:12 +0800

    Initial Commit

---
+++ video/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  video
+//
+//  Created by Qeebu on 13-12-16.
+//  Copyright (c) 2013年 Qeebu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
